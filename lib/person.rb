bdd615eef007c368555c551be4af436134bf524e
Lauren Days <lmchambers0420@gmail.com>
2019-06-06 21:39:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   #your code here
   
   def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="),value)
+    attributes.each {|key, value| self.send(("#{key}="),value)}
   end 
 end
